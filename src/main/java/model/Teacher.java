dd9575a27fe7811750019047aff15c9b9165525a
HMonique <moniquehl@outlook.com>
2024-09-18 08:48:36 -0400

    sixth commit

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -2,8 +2,10 @@ package model;
 
 import jakarta.persistence.*;
 import model.Department;
+
 import java.io.Serial;
 import java.io.Serializable;
+
 @Entity
 @Table
 public class Teacher implements Serializable {
@@ -15,15 +17,31 @@ public class Teacher implements Serializable {
     private String salary;
     private String teacherName;
 
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
     public Teacher( String salary, String teacherName) {
         super();
         this.salary = salary;
         this.teacherName = teacherName;    }
     public Teacher() {}
+
     public Teacher(String salary, String teacherName, Department department) {
         this.salary = salary;
         this.teacherName = teacherName;
     }
+
+
     public int getTeacherId() {
         return teacherId;
     }
@@ -42,4 +60,5 @@ public class Teacher implements Serializable {
     public void setTeacherName(String teacherName) {
         this.teacherName = teacherName;    }
 }
+
 
